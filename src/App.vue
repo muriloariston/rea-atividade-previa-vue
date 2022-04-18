c14a5a0b76d7cb150ec54afa7ba4e65cd9815bdd
murilo.ariston <moariston@gmail.com>
2022-04-18 19:53:34 -0300

    atividade vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="container">
+    <Filtrar />
+  </div>
+</template>
+
+<script>
+import Filtrar from "./components/Filtrar";
+
+export default {
+  name: "App",
+  components: { Filtrar },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Filtrar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app" class="container">
+    <div class="card">
+      <div class="card-header">
+        <div class="form-row">
+          <div class="form-group">
+            <label for="inputTexto"
+              >Texto (pressione [Enter] para filtrar os números)</label
+            >
+            <input
+              autofocus
+              @keyup.enter="filterText($event.target.value)"
+              type="text"
+              name="inputTexto"
+              class="form-control"
+              placeholder="Novo Texto"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Números:</h5>
+        <div>{{ numeros }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Filtrar",
+  data() {
+    return { numeros: "" };
+  },
+  methods: {
+    filterText: function (text) {
+      let numb = text.match(/\d/g);
+      numb = numb.join("");
+      this.numeros = numb;
+    },
+  },
+};
+</script>
